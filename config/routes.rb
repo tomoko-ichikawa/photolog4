c6cc7cbcb6b19743bae593689a6172f426b92784
tomoko ichikawa <takator@yahoo.co.jp>
2019-07-02 08:53:26 +0900

    アプリ作成

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  root 'photos#index'
+  devise_for :users
+    resources :photos do
+        collection do
+            post:confirm
+        end
+    end
+  resources :users
+end
