08717c6df4ff9481da1a8f1e561a93ddcfa30ff5
tomoko ichikawa <takator@yahoo.co.jp>
2019-07-02 19:08:36 +0900

    プロフィール画像設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   protected
   def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile, :avatar, :avatar_cache, :remove_avatar])
   end
   
   def after_sign_in_path_for(resource)
