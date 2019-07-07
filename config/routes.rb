055b511187d8df0e46270fde9cd4f3dc28bb7cfc
tomoko ichikawa <takator@yahoo.co.jp>
2019-07-07 11:51:58 +0900

    メール通知設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,14 @@ Rails.application.routes.draw do
   get 'favorites/destroy'
 
   root 'photos#index'
-  devise_for :users
+  devise_for :users, :controllers => {
+      :registrations => 'users/registrations'
+  }
   resources :photos do
       collection do
             post:confirm
       end
   end
-  devise_for :users, :controllers => {
-      :registrations => 'users/registrations'
-  }
+ 
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
